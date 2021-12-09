669fedf819ac20af25e6e2accc55857004f893ff
Jordan Reger <jordanreger@gmail.com>
2021-12-08 21:47:09 -0500

    pkgpull

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: deno run --allow-net=:${PORT} --allow-read --unstable server.ts --port=${PORT}
--- server.ts
+++ server.ts
@@ -1,94 +1,38 @@
-import { parse } from 'https://deno.land/std/flags/mod.ts';
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-const { args } = Deno;
-const DEFAULT_PORT = 6969;
-const argPort = parse(args).port;
+async function handler(req: Request): Promise<any> {
+  let path = (function(){
+    let url = req.url.split("/"), path = `/${url[url.length - 1]}`;
+    return path;
+  })();
+  let file;
+  let ct;
 
-const server = Deno.listen({ port: argPort ? Number(argPort) : DEFAULT_PORT });
-console.log("./ → http://localhost:6969");
+  switch(path){
+    // PAGES
+    case '/':
+      file = Deno.readFile("./src/index.html");
+      ct = "text/html; charset=UTF-8";
+      break;
 
-while (true) {
-  try {
-    const conn = await server.accept();
+    // SETUP
+    case '/robots.txt':
+      file = "User-agent: * Disallow:";
+      ct = "text/plain";
+      break;
 
-    (async () => {
-      const http = Deno.serveHttp(conn);
-      for await (const request of http) {
-
-        let path = (function(){
-          let paths = request.request.url.split("/");
-          paths.splice(0, 3);
-          let path = "/" + paths.join("/");
-          if(path !== ""){
-            return path;
-          } else {
-            return "/";
-          }
-        })();
-        let file, ct, f, res;
-
-        switch(path){
-          /* PAGES */
-          case '/':
-            f = true;
-            file = Deno.readFile("./src/index.html");
-            ct = "text/html; charset=UTF-8";
-            break;
-
-          /* OTHER */
-
-          /* ASSETS */
-          case '/manifest.webmanifest':
-            f = true;
-            file = Deno.readFile("./src/utils/manifest.webmanifest");
-            ct = "application/manifest+json";
-            break;
-          case '/service-worker.js':
-            f = true;
-            file = Deno.readFile("./src/utils/serviceWorker.js");
-            ct = "text/javascript";
-            break;
-          case '/main.css':
-            f = true;
-            file = Deno.readFile("./src/utils/main.css");
-            ct = "text/css";
-            break;
-          case '/base_logo.png':
-            f = true;
-            file = Deno.readFile("./src/utils/base_logo.png");
-            ct = "image/png";
-            break;
-
-          /* DEPENDENCIES */
-          case '/pkgSearchBar.js':
-            f = true;
-            file = Deno.readFile("./src/bundled/pkgSearchBar.js");
-            ct = "text/javascript";
-            break;
-
-          /* DEFAULT */
-          default:
-            f = true;
-            file = Deno.readFile("./src/utils/404.html");
-            ct = "text/html; charset=UTF-8";
-        }
-
-        if(f) {
-            res = new Response(await file, {
-            headers: {
-              "content-type": ct,
-            },
-          });
-        } else {
-          let blob = new Blob();
-          res = new Response(blob, { "status" : 200 });
-        }
-
-        await request.respondWith(res);
-      }
-    })();
-
-  } catch (err) {
-    break;
+    default:
+      file = Deno.readFile("./src/404.html");
+      ct = "text/html; charset=UTF-8";
   }
+
+  let res = new Response(await file, {
+    headers: {
+      "content-type": ct,
+    },
+  });
+
+  return res;
 }
+
+await serve(handler);
