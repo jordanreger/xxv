8b40282448782e2f48dae6d6b2746297e7208110
Jordan Reger <jordanreger@gmail.com>
2021-12-18 11:48:58 -0500

    we have gone un-native! pwa for now seems to do the trick

---
+++ server.ts
@@ -0,0 +1,66 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+async function handler(req: Request): Promise<any> {
+  let path = (function(){
+    let url = req.url.split("/"), path = `/${url[url.length - 1]}`;
+    return path;
+  })();
+
+  let file, ct, f, r;
+
+  switch(path){
+    // PAGES
+    case '/':
+      f = true, r = "";
+      file = Deno.readFile("./src/xxv.html");
+      ct = "text/html; charset=UTF-8";
+      break;
+
+    case '/robots.txt':
+      f = true, r = "";
+      file = "User-agent: * Disallow:";
+      ct = "text/plain";
+      break;
+    case '/manifest.webmanifest':
+      f = true, r = "";
+      file = Deno.readFile("./src/utils/manifest.webmanifest");
+      ct = "application/manifest+json";
+      break;
+    case '/service-worker.js':
+      f = true, r = "";
+      file = Deno.readFile("./src/utils/serviceWorker.js");
+      ct = "text/javascript";
+      break;
+    case '/main.css':
+      f = true, r = "";
+      file = Deno.readFile("./src/utils/main.css");
+      ct = "text/css";
+      break;
+    case '/base_logo.png':
+      f = true, r = "";
+      file = Deno.readFile("./src/utils/base_logo.png");
+      ct = "image/png";
+      break;
+
+    default:
+      f = true, r = "";
+      file = Deno.readFile("./src/utils/404.html");
+      ct = "text/html; charset=UTF-8";
+  }
+
+  let res;
+
+  if(f){
+    res = new Response(await file, {
+      headers: {
+        "content-type": ct,
+      },
+    });
+  } else {
+    res = Response.redirect(r, 302);
+  }
+
+  return res;
+}
+
+await serve(handler, { addr: ":42069" });
