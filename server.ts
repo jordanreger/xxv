47c1b3453ec919341d3998030beb8a05b0267f14
Jordan Reger <jordanreger@gmail.com>
2021-12-28 11:08:18 -0500

    update std

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 import { route } from "./router.ts";
 
 async function handler(req: Request): Promise<any> {
