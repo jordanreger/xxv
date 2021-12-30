db092367eae15bc47006c41829bc53547c430618
Jordan Reger <jordanreger@gmail.com>
2021-12-30 11:32:45 -0500

    hopefully fix the base64 system

--- server.ts
+++ server.ts
@@ -43,7 +43,7 @@ async function handler(req: Request): Promise<any> {
 
     case '/module':
       const data = String(params.get("data"));
-      file = `<object data="data:text/html;base64,${decodeURI(data)}" style="position:absolute;left:0;top:0;width:100%;height:100%;"></object>`;
+      file = `<object data="data:text/html;base64,${decodeURIComponent(data)}" style="position:absolute;left:0;top:0;width:100%;height:100%;"></object>`;
       f = true, r = "";
       ct = "text/html; charset=UTF-8";
       break;
