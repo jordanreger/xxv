690782a7be504e6cfff3b0cf34b18d01608a21e6
Jordan Reger <jordanreger@gmail.com>
2022-01-03 14:49:11 -0500

    oops wrong file name

--- process/server.ts
+++ process/server.ts
@@ -10,7 +10,7 @@ async function handler(req: Request): Promise<any> {
 
   switch(path){
     case '/':
-      tr = true, rb = await file("./process/src/index.html"), ct = "text/html; charset=UTF-8";
+      tr = true, rb = await file("./process/src/xxv.html"), ct = "text/html; charset=UTF-8";
       break;
 
     case '/robots.txt':
