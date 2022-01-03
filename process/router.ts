dd03af17357a9ba5317c1bd823e9cd550e8f31e9
Jordan Reger <jordanreger@gmail.com>
2022-01-03 14:44:06 -0500

    land~fix logo process~update server

--- process/router.ts
+++
@@ -1,8 +0,0 @@
-export const route = (route:string, path: string) => {
-  let regexRoute = new RegExp(route, "gmi");
-  if(regexRoute.test(path)){
-    return path;
-  } else {
-    return null;
-  }
-}
--- process/server.ts
+++ process/server.ts
@@ -1,72 +1,51 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-import { route } from "./router.ts";
 
 async function handler(req: Request): Promise<any> {
   let url = new URL(req.url);
   let path = url.pathname, params = new URLSearchParams(url.search);
+  const route = (route:string) => { let regexRoute = new RegExp(route, "gmi"); if(regexRoute.test(path)){ return path } else { return null }}
+  const file = async (fp:string) => { let d = new TextDecoder("utf-8"); return d.decode(await Deno.readFile(fp))}
 
-  let file, ct, f, r;
+  let tr, rb, ct = "";
 
   switch(path){
-    // PAGES
     case '/':
-      f = true, r = "";
-      file = Deno.readFile("./process/src/xxv.html");
-      ct = "text/html; charset=UTF-8";
+      tr = true, rb = await file("./process/src/index.html"), ct = "text/html; charset=UTF-8";
       break;
 
     case '/robots.txt':
-      f = true, r = "";
-      file = "User-agent: * Disallow:";
-      ct = "text/plain";
+      tr = true, rb = "User-agent: * Disallow:", ct = "text/plain";
       break;
     case '/manifest.webmanifest':
-      f = true, r = "";
-      file = Deno.readFile("./process/src/utils/manifest.webmanifest");
-      ct = "application/manifest+json";
+      tr = true, rb = await file("./process/src/utils/manifest.webmanifest"), ct = "application/manifest+json";
       break;
     case '/service-worker.js':
-      f = true, r = "";
-      file = Deno.readFile("./process/src/utils/serviceWorker.js");
-      ct = "text/javascript";
+      tr = true, rb = await file("./process/src/utils/serviceWorker.js"), ct = "text/javascript";
       break;
     case '/main.css':
-      f = true, r = "";
-      file = Deno.readFile("./process/src/utils/main.css");
-      ct = "text/css";
+      tr = true, rb = await file("./process/src/utils/main.css"), ct = "text/css";
       break;
     case '/xxvlogo.png':
-      f = true, r = "";
-      file = Deno.readFile("./process/src/utils/xxvlogo.png");
-      ct = "image/png";
+      tr = true, rb = await file("./process/src/utils/xxvlogo.png"), ct = "image/png";
       break;
 
     case '/module':
       let data = String(params.get("data"));
       data = data.replace(" ", "+");
-      file = `<object data="data:text/html;base64,${decodeURIComponent(data)}" style="position:absolute;left:0;top:0;width:100%;height:100%;"></object>`;
-      f = true, r = "";
-      ct = "text/html; charset=UTF-8";
+      tr = true, rb = `<object data="data:text/html;base64,${decodeURIComponent(data)}" style="position:absolute;left:0;top:0;width:100%;height:100%;"></object>`, ct = "text/html; charset=UTF-8";
       break;
 
     default:
-      f = true, r = "";
-      file = Deno.readFile("./process/src/utils/404.html");
-      ct = "text/html; charset=UTF-8";
+      tr = true, rb = await file("./process/src/utils/404.html"), ct = "text/html; charset=UTF-8";
   }
 
   let res;
 
-  if(f){
-    res = new Response(await file, {
-      headers: {
-        "content-type": ct,
-      },
-    });
+  if(tr){
+    res = new Response(await rb, { headers: { "content-type": ct } });
   } else {
-    res = Response.redirect(r, 302);
+    res = Response.redirect(rb, 302);
   }
-
   return res;
 }
 
